f0ffeeededd72381c8a40c10a5229ea31a3b0c62
Crodic <113219638+Crodic@users.noreply.github.com>
2023-10-28 13:46:44 +0700

    searchData and filter Data

--- api/v1/product/index.php
+++ api/v1/product/index.php
@@ -30,6 +30,10 @@ if (!isset($action)) {
     exit;
 }
 
+$titleSearchData = null;
+if (isset($_POST["title"])) {
+    $titleSearchData = $_POST["title"];
+}
 
 switch ($action) {
     case "create":
@@ -157,16 +161,19 @@ switch ($action) {
                 }
                 // SQL PHÂN TRANG
                 $sqlSelect = "SELECT * FROM product";
-                $paginate = "LIMIT :skip, :limit";
+                $paginate = "LIMIT :skip, :limit;";
                 $where = "WHERE 1";
                 if (isset($_GET["category"])) {
-                    $where = "WHERE cid = :cid";
+                    $where .= " AND cid = :cid";
                 }
-                $cid = $_GET["category"];
-                $pstm = $conn->prepare("$sqlSelect $where $paginate");
+                $query = "$sqlSelect $where $paginate";
+                $pstm = $conn->prepare($query);
                 $pstm->bindValue(":skip", (int) trim($skip), PDO::PARAM_INT);
                 $pstm->bindValue(":limit", (int) trim($limit), PDO::PARAM_INT);
-                $pstm->bindParam(":cid", $cid);
+                if (isset($_GET["category"])) {
+                    $cid = $_GET["category"];
+                    $pstm->bindValue(":cid", (int) trim($cid), PDO::PARAM_INT);
+                }
                 $pstm->execute();
                 $results = $pstm->fetchAll(PDO::FETCH_ASSOC);
                 $products = []; // Mảng Chứa Các Sản Phẩm
@@ -213,6 +220,103 @@ switch ($action) {
                     "status" => false,
                     "statusCode" => 400,
                     "msg" => "Lấy Danh Sách Sản Phẩm Thất Bại.",
+                    "Error" => $th->getMessage(),
+                ]);
+            }
+        } else {
+            http_response_code(404);
+            echo json_encode([
+                "status" => false,
+                "statusCode" => 404,
+                "msg" => "Không Tìm Thấy API Tương Ứng",
+            ]);
+        }
+        break;
+    case "search":
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Thuật Toán Phân Trang
+            $page = isset($_GET["page"]) ? $_GET["page"] : 1;
+            $limit = isset($_GET["limit"]) ? $_GET["limit"] : 20;
+            $skip = (int) ($page - 1) * $limit;
+            try {
+                // Lấy Ra Tổng Số Phần Tử Trong Table
+                $total = 0;
+                $pstm = $conn->prepare("SELECT * FROM product");
+                $pstm->execute();
+                $allData = $pstm->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($allData as $count) {
+                    $total = $total + 1;
+                }
+                // SQL PHÂN TRANG
+                $sqlSelect = "SELECT * FROM product";
+                $paginate = "LIMIT :skip, :limit;";
+                $where = "WHERE 1";
+                if (isset($_GET["category"])) {
+                    $where .= " AND cid = :cid";
+                }
+                if (isset($_POST["title"])) {
+                    $where .= " AND title LIKE :title";
+                }
+                $query = "$sqlSelect $where $paginate";
+                $pstm = $conn->prepare($query);
+                $pstm->bindValue(":skip", (int) trim($skip), PDO::PARAM_INT);
+                $pstm->bindValue(":limit", (int) trim($limit), PDO::PARAM_INT);
+                if (isset($_GET["category"])) {
+                    $cid = $_GET["category"];
+                    $pstm->bindValue(":cid", (int) trim($cid), PDO::PARAM_INT);
+                }
+                if (isset($_POST["title"])) {
+                    $title = $_POST["title"];
+                    $dataTitle = "%$title%";
+                    $pstm->bindParam(":title", $dataTitle);
+                }
+                $pstm->execute();
+                $results = $pstm->fetchAll(PDO::FETCH_ASSOC);
+                $products = []; // Mảng Chứa Các Sản Phẩm
+                foreach ($results as $data) {
+                    // Object Sản Phẩm
+                    $dataProduct = [
+                        "title" => $data["title"],
+                        "price" => $data["price"],
+                        "discount" => $data["discount"],
+                        "description" => $data["description_product"],
+                        "slug" => $data["slug"],
+                        "pid" => $data["pid"],
+                        "cid" => $data["cid"],
+                        "isDeleted" => $data["isDeleted"],
+                    ];
+                    // Select Hình Ảnh Tử Table imagesproduct từ pid vừa lấy dc
+                    $pstm = $conn->prepare("SELECT * FROM imagesproduct WHERE pid = :pid");
+                    $pstm->execute(array(
+                        "pid" => $data["pid"]
+                    ));
+                    // Duyệt Qua Từng Hình Mảng
+                    $images = $pstm->fetchAll(PDO::FETCH_ASSOC);
+                    $listImages = []; // Mảng Chứa Các Hình Ảnh
+                    foreach ($images as $image) {
+                        $dataImg = $image["description"]; // Lấy URL hình ảnh
+                        array_push($listImages, $dataImg); // Thêm Vào mảng chứa hình ảnh
+                    }
+                    $dataProduct["images"] = $listImages; // Thêm Hình Ảnh Vào Mảng dataProduct có key là images => $dataProduct["images"]
+                    array_push($products, $dataProduct); // Đẩy $dataProduct vào mảng các sản phẩm
+                }
+                $totalPage = ceil($total / $limit); // Tính Tổng Số Lượng Page Để Chứa Hết Các Bản Ghi Trong Table Product
+                http_response_code(200);
+                echo json_encode([
+                    "status" => true,
+                    "statusCode" => 200,
+                    "page" => (int) $page,
+                    "total" => $total,
+                    "totalPage" => $totalPage,
+                    "products" => $products
+                ]);
+            } catch (\Throwable $th) {
+                http_response_code(400);
+                echo json_encode([
+                    "status" => false,
+                    "statusCode" => 400,
+                    "msg" => "Lấy Danh Sách Sản Phẩm Thất Bại.",
+                    "Error" => $th->getMessage(),
                 ]);
             }
         } else {
