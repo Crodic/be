d62f776e464aac85390421a9fa1f12b893e47907
Crodic <113219638+Crodic@users.noreply.github.com>
2023-11-30 13:12:13 +0700

    end project

--- api/v1/feedback/index.php
+++ api/v1/feedback/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include_once "../../../utils/Common.php";
 include_once "../../../configs/DBContext.php";
 header("Access-Control-Allow-Origin: *");
@@ -18,3 +18,74 @@ $common = new Common();
 $conn = new DBContext();
 $conn = $conn->Connection();
 $action = $_GET["action"];
+
+
+switch ($action) {
+    case "feedback":
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $fname = $_POST["firstname"];
+            $lname = $_POST["lastname"];
+            $email = $_POST["email"];
+            $phone = $_POST["phone"];
+            $note = $_POST["note"];
+            $timestamp = time(); // Lấy giá trị thời gian hiện tại dưới dạng số giây
+            $currentTime = date('Y-m-d H:i:s', $timestamp); // Chuyển đổi thành định dạng datetime
+
+
+            if (!isset($_POST["firstname"]) || !isset($_POST["lastname"]) || !isset($_POST["email"]) || !isset($_POST["phone"]) || !isset($_POST["note"])) {
+                http_response_code(400);
+                echo json_encode(
+                    [
+                        "status" => false,
+                        "statusCode" => 400,
+                        "msg" => "Thiếu thông tin cần thiết"
+                    ]
+                );
+                exit;
+            }
+
+
+            try {
+                $pstm = $conn->prepare("INSERT INTO feedback VALUES(null, :firstname, :lastname, :email, :phone_number, 1, :note, 0, :createdAt, :updatedAt)");
+                $pstm->execute(array(
+                    "firstname" => $fname,
+                    "lastname" => $lname,
+                    "email" => $email,
+                    "phone_number" => $phone,
+                    "note" => $note,
+                    "createdAt" => $currentTime,
+                    "updatedAt" => $currentTime
+                ));
+                if ($pstm->rowCount() > 0) {
+                    http_response_code(200);
+                    echo json_encode(
+                        [
+                            "status" => true,
+                            "statusCode" => 200,
+                            "msg" => "Cảm ơn bạn đã đóng góp ý kiến"
+                        ]
+                    );
+                } else {
+                    http_response_code(400);
+                    echo json_encode(
+                        [
+                            "status" => true,
+                            "statusCode" => 400,
+                            "msg" => "Vui Lòng Thử Lại Sau !!!"
+                        ]
+                    );
+                }
+            } catch (\Throwable $th) {
+                http_response_code(500);
+                echo json_encode([
+                    "status" => false,
+                    "statusCode" => 500,
+                    "msg" => "Lỗi Truy Cập Vào SQL",
+                    "error" => $th->getMessage(),
+                ]);
+            }
+        }
+        break;
+    default:
+        return;
+}
